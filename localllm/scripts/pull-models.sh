ccf52e06c072148958fd87ca85463bf7ef7ae19d
Ercenk Keresteci <ercenk@ercenk.com>
2024-03-16 12:25:52 -0700

    Right before merge

--- localllm/scripts/pull-models.sh
+++ localllm/scripts/pull-models.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo "Starting Ollama..."
 ollama serve &
 
+echo "Waiting for Ollama to start..."
 sleep 10
 
 IFS=',' read -ra MODELS <<< "$1"
 for i in "${MODELS[@]}"; do
+    echo "Pulling model $i..." 
     ollama pull $i
-    ollama run $i &
 done
-
-wait
